cd972f452ab1f68a917170470a5d565472135f33
libw <3180234769@qq.com>
2017-10-10 15:03:33 +0800

    出金

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import configureStore from './store/configureStore' 
 import createRouter from './routes'
 import { Provider } from 'react-redux';
-
+import 'antd-mobile/dist/antd-mobile.css';
 
 const store = configureStore(window.__initState__)
 console.log(store.getState())
@@ -13,7 +13,7 @@ class App extends Component {
     return (
 		<Provider store={store} >
 		    {createRouter()}
-	  </Provider>
+	    </Provider>
     );
   }
 }
--- src/containers/outGold/index.js
+++ src/containers/outGold/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from  "./index.css"
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { List, InputItem, Button } from 'antd-mobile';
 
 
 class OutGold extends React.Component{
@@ -11,12 +12,67 @@ class OutGold extends React.Component{
         }
 	}
 	render(){
-			console.log('2222',this.props.foreignExchange)
-			return(
-				<div className={style.wrap} >foreignExchange</div>
-			)
+        console.log('2222',this.props.foreignExchange);
 
-		}
+        return(
+            <div className={style.wrap} >
+                <div className="part1">
+                    <List >
+                        <InputItem
+                            defaultValue={this.props.foreignExchange.bankName+" "+this.props.foreignExchange.bankNo}
+                            type="bankCard"
+                            editable={false}
+                            style={{textAlign:"right"}}
+                        >出金账户</InputItem>
+                        <InputItem
+                            defaultValue={"$ "+this.props.foreignExchange.accountBalance}
+                            type="bankCard"
+                            editable={false}
+                            className={style.Apphh}
+                            style={{textAlign:"right"}}
+                        >账户余额</InputItem>
+                    </List>
+                </div>
+
+
+                <List >
+                    <InputItem
+                        placeholder="输入金额，最低50美元"
+                        extra="$"
+                        style={{textAlign:"right"}}
+                    >价格</InputItem>
+                </List>
+                <div className="tip">
+                        <span className={style.left}>
+                            合人民币：<span></span>
+                        </span>
+                    <span className="right">
+                            当前汇率：{this.props.foreignExchange.exchangeRate}
+                        </span>
+                </div>
+                <List >
+                    <InputItem
+                        type="password"
+                        placeholder="输入支付密码"
+                        style={{textAlign:"right"}}
+                    >支付密码</InputItem>
+
+                </List>
+                <div className="tip">
+                        <span className={style.left}>
+                            注：支付密码即登录密码
+                        </span>
+                    <span className="right">
+                        <a href="javascript:void (0)">
+                            忘记密码？
+                        </a>
+                    </span>
+                </div>
+
+            </div>
+        )
+
+	}
 }
 
 function mapStateToProps(state, props) {
