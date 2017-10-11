e176b92a141169adf1449006a32bf402c8c1a55d
libw <3180234769@qq.com>
2017-10-11 16:40:21 +0800

    忘记密码

--- src/containers/forgetPwd/index.js
+++ src/containers/forgetPwd/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from "./index.css"
 import {connect} from 'react-redux'
-import { List,InputItem,Button,WingBlank} from 'antd-mobile';
+import { List,InputItem,Button,WingBlank,Picker} from 'antd-mobile';
 
 class ForgetPwd extends React.Component {
     constructor(props) {
@@ -10,8 +10,73 @@ class ForgetPwd extends React.Component {
     }
 
     render() {
+        const quhao=[
+            {
+                value:1,
+                label:"中国大陆  +86"
+            },{
+                value:2,
+                label:"中国台湾  +87"
+            },{
+                value:3,
+                label:"中国香港  +88"
+            },
+
+        ]
         return (
             <div className={style.wrap}>
+
+
+                    <section className={style.content}>
+                        <span className={style.title}>
+                        忘记密码？
+                        </span>
+                        <div className={style.selphone}>
+                            <div className={style.qh}>
+                                <Picker data={quhao} cols={1} className="forss">
+                                    <List.Item arrow="horizontal"> </List.Item>
+                                </Picker>
+                            </div>
+                            <div className={style.line}></div>
+                            <div className={style.phone}>
+                                <List>
+                                    <InputItem placeholder="请输入手机号" type="number"></InputItem>
+                                </List>
+                            </div>
+                        </div>
+                        <div className={style.selphone}>
+                            <div className={style.tu}>
+                                <List>
+                                    <InputItem placeholder="请输入图形验证码" type="text"></InputItem>
+                                </List>
+
+                            </div>
+                            <img className={style.tuxing} src="http://reso2.yiihuu.com/1331436-z.jpg" alt=""/>
+                        </div>
+                        <div className={style.selphone}>
+                            <div className={style.tu}>
+                                <List>
+                                    <InputItem placeholder="请输入验证码" type="text" extra="获取验证码"></InputItem>
+                                </List>
+
+                            </div>
+                            <div className={style.lline}></div>
+
+                        </div>
+                        <div className={style.selphone}>
+                            <div className={style.tu}>
+                                <List>
+                                    <InputItem type="password" placeholder='请设置6-20位密码'></InputItem>
+                                </List>
+
+                            </div>
+                        </div>
+                        <div className={style.button}>
+                            <Button type="primary">
+                                确认修改密码
+                            </Button>
+                        </div>
+                    </section>
 
             </div>
         )
